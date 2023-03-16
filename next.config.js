4a94ca367b5507ac1517f875437e2b1ff957b721
juandavidGF <davad701@gmail.com>
2023-03-16 16:49:29 -0500

    delete make question

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
+const { i18n } = require('./next-i18next.config');
+
+
 const nextConfig = {
+	i18n,
   reactStrictMode: true,
 	async headers() {
     return [
@@ -8,7 +12,7 @@ const nextConfig = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Origin", value: "same-origin" },
           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
         ]
