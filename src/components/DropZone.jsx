d69b33ff5a414ec6ec9bc81bc0fd5ca981738d2d
juandavidGF <davad701@gmail.com>
2023-03-21 12:58:41 -0500

    let animation well enough

--- src/components/DropZone.jsx
+++ src/components/DropZone.jsx
@@ -6,7 +6,7 @@ import sleep from "../utils/sleep";
 const ImageDropZone = ({ onVideoChange }) => {
 
 	const [videoState, setVideoState] = useState("Upload the Video");
-	const [loading, setLoading] = useState(false);
+	const [loading, setLoading] = useState(true);
 
   const handleDrop = async (e) => {
     e.preventDefault();
